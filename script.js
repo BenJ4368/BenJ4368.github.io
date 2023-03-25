4f549e3030f670396a5a8df3791bcd37ad18a8cd
Benjamin Gaertner <benjaming@outlook.fr>
2023-03-25 17:50:36 +0100

    Animation done

--- script.js
+++ script.js
@@ -9,75 +9,56 @@ for (var j = 0; j < paths.length; j++) {
 
 for (var i = 0; i < paths.length; i++) {
 	let elementLength = paths[i].getTotalLength();
-	let writingTime = (elementLength/totalLength) + 0;
+	let writingTime = (elementLength/totalLength) ;
 	let animDelay = 0.5;
 	switch (i) {
 		case 0: //Barre du B
 			paths[i].style.animation = "draw "+ (writingTime * 6) + "s linear forwards " + (animDelay + 0) + "s";
 			break;
-		default:
-			prevElemLength += paths[i - 1].getTotalLength();
-			animDelay = ((prevElemLength*4)/totalLength);
-			console.info('jepasse');
 		case 1: // Boucle du B
-			paths[i].style.animation = "draw " + (writingTime * 2) + "s linear forwards " + (animDelay + 0.4) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 2) + "s linear forwards " + (animDelay + 0.2) + "s";
 			break;
 		case 2: //en
-			paths[i].style.animation = "draw " + (writingTime * 4) + "s linear forwards " + (animDelay + 1) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 4) + "s linear forwards " + (animDelay + 0.7) + "s";
 			break;
 		case 3: //j
-			paths[i].style.animation = "draw " + (writingTime * 1) + "s linear forwards " + (animDelay + 1.4) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 1) + "s linear forwards " + (animDelay + 1.1) + "s";
 			break;
 		case 4: //am
-			paths[i].style.animation = "draw " + (writingTime * 10) + "s linear forwards " + (animDelay + 1.8) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 10) + "s linear forwards " + (animDelay + 1.4) + "s";
 			break;
 		case 5: // i
-			paths[i].style.animation = "draw " + (writingTime * 8) + "s linear forwards " + (animDelay + 2.6) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 8) + "s linear forwards " + (animDelay + 2.1) + "s";
 			break;
 		case 6: //n
-			paths[i].style.animation = "draw " + (writingTime * 8) + "s linear forwards " + (animDelay + 2.8) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 8) + "s linear forwards " + (animDelay + 2.3) + "s";
 			break;
 		case 7: // point du i
-			paths[i].style.animation = "draw " + (writingTime * 10) + "s linear forwards " + (animDelay + 2.4) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 10) + "s linear forwards " + (animDelay + 2.7) + "s";
 			break;
 		case 8: //G
-			paths[i].style.animation = "draw " + (writingTime * 2) + "s linear forwards " + (animDelay + 2.6) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 2) + "s linear forwards " + (animDelay + 3) + "s";
 			break;
 		case 9: //ae
-			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 2.8) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 3.5) + "s";
 			break;
 		case 10: //r
-			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 3) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 4.2) + "s";
 			break;
 		case 11: // pied du t
-			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 3.2) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 4.5) + "s";
 			break;
 		case 12: //ne
-			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 3.4) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 4.8) + "s";
 			break;
 		case 13: //r
-			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 3.6) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 6) + "s linear forwards " + (animDelay + 5.2) + "s";
 			break;
 		case 14: //barre du t
-			paths[i].style.animation = "draw " + (writingTime * 4) + "s linear forwards " + (animDelay + 3.8) + "s";
-			console.log(`Précédent path[${i - 1}]; length: ${prevElemLength}`)
+			paths[i].style.animation = "draw " + (writingTime * 3) + "s linear forwards " + (animDelay + 5.5) + "s";
 			break;
 
 	}
 	paths[i].style.strokeDasharray = elementLength;
 	paths[i].style.strokeDashoffset = elementLength;
-	console.log(`path[${i}]; length: ${elementLength}`)
 }
